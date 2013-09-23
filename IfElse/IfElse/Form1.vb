0fcce5cea5546d7a2007b9350544dd84debdb2e6
idsc3101 <idsc3101@gmail.com>
2013-09-23 15:17:55 -0500

    Modified

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,17 +4,33 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
 
-        Dim num1, num2, largerNum As Double
+        Dim num1, num2 As Double
+
+        'if user didn't type the number, elinate him
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+            MessageBox.Show("Error: please type a number")
+            Return
+        End If
 
         num1 = CDbl(txtFirstNum.Text)
         num2 = CDbl(txtSecondNum.Text)
 
+        'if user didn't type positive number, elinate him
+        If num1 < 0 Or num2 < 0 Then
+            MessageBox.Show("Error: Please type a positive number")
+        End If
+
+        'sterile area
         If num1 > num2 Then
             txtResult.Text = "The larger number is" & num1
         ElseIf num1 < num2 Then
             txtResult.Text = "The larger number is" & num2
         Else : txtResult.Text = "They are equal"
         End If
+
+
+
+
 
 
     End Sub
